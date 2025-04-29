e4d4a62cb7357d8276947786b7441ba720ff4ff2
Javier Cañas <alu0101333021@ull.edu.es>
2025-04-29 12:35:20 +0000

    Merchant Model

--- src/Peticiones/Merchants.ts
+++ src/Peticiones/Merchants.ts
@@ -1,10 +1,99 @@
-import Mercader from '../Entidades/Mercader.js';
+import express from 'express';
+import Merchant from '../Modelos/MerchantModel.js';
 
-import { Router, Request, Response } from 'express';
-const router = Router();
+const router = express.Router();
 
-router.get('/', (_, res) => {
-    res.send('Notes');
+// Crear
+router.post('/', async (req, res) => {
+  const merchant = new Merchant(req.body);
+
+  try {
+    await merchant.save();
+    res.send(merchant);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+// Obtener todos
+router.get('/', async (_, res) => {
+  try {
+    const merchant = await Merchant.find();
+    res.send(merchant);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+// Obtener
+router.get('/', async (req, res) => {
+
+  let filter;
+  if (req.query.nombre) {
+    filter = { nombre: req.query.nombre.toString() };
+  } else if (req.query._id) {
+    filter = { _id: req.query._id };
+  }
+  else {
+    filter = {};
+  }
+
+  try {
+    const merchant = await Merchant.find(filter);
+
+    if (merchant.length !== 0) {
+      res.send(merchant);
+    } else {
+      res.status(404).send();
+    }
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+
+// Modificar
+router.patch('/', async (req, res) => {
+  const filter = req.body.nombre ? { nombre: req.body.nombre.toString() } : { _id: req.body._id };
+  if(!filter) {
+    res.status(400).send({ error: 'Falta el nombre o id' });
+  }
+
+  try {
+    const merchant = await Merchant.findOneAndUpdate({ filter }, req.body, { new: true, runValidators: true });
+    if(!merchant) {
+      res.status(404).send({ error: 'No encontrado' });
+    }
+
+    res.send(merchant);
+  } catch (error) {
+    res.status(500).send({ error });
+  }
+});
+
+
+// Borrar
+router.delete('/', async (req, res) => {
+  const filter = req.body.nombre ? { nombre: req.body.nombre.toString() } : { _id: req.body._id };
+  if(!filter) {
+    res.status(400).send({ error: 'Falta el nombre o id' });
+ }
+
+  try {
+
+    const merchant = await Merchant.findOneAndDelete({ filter });
+    if(!merchant) {
+      res.status(404).send({ error: 'No encontrado' });
+    }
+
+    res.send({ mensaje: 'Eliminado' });
+  } catch (error) {
+    res.status(500).send({ error });
+  }
+});
+
+router.all('/{*splat}', (_, res) => {
+  res.status(501).send();
 });
 
 export default router;
